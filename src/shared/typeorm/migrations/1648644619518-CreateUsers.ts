d8d8d623eeb0579739c99953ce4b619ef8a866e8
eldersoon <eldersondagama@hotmail.com>
2022-03-30 16:13:36 -0300

    is null cpf and email

--- src/shared/typeorm/migrations/1648644619518-CreateUsers.ts
+++ src/shared/typeorm/migrations/1648644619518-CreateUsers.ts
@@ -1,57 +1,60 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class CreateUsers1648644619518 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     await queryRunner.createTable(
       new Table({
-        name: "users",
+        name: 'users',
         columns: [
           {
-            name: "id",
-            type: "uuid",
+            name: 'id',
+            type: 'uuid',
             isPrimary: true,
-            generationStrategy: "uuid",
-            default: "uuid_generate_v4()",
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
-            name: "nome",
-            type: "varchar",
+            name: 'nome',
+            type: 'varchar',
           },
           {
-            name: "cpf",
-            type: "varchar",
+            name: 'cpf',
+            type: 'varchar',
+            isNullable: true,
+            isUnique: true,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'senha',
+            type: 'varchar',
+          },
+          {
+            name: 'telefone',
+            type: 'varchar',
             isNullable: true,
           },
           {
-            name: "email",
-            type: "varchar",
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
           },
           {
-            name: "senha",
-            type: "varchar",
-          },
-          {
-            name: "telefone",
-            type: "varchar",
-            isNullable: true,
-          },
-          {
-            name: "created_at",
-            type: "timestamp with time zone",
-            default: "now()",
-          },
-          {
-            name: "updated_at",
-            type: "timestamp with time zone",
-            default: "now()",
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
           },
         ],
       })
-    );
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("users");
+    await queryRunner.dropTable('users')
+    await queryRunner.query('DROP EXTENSION IF EXISTS "uuid-ossp"')
   }
 }
