48ccb955b1550adf49bee29ead847a3bd7636a60
Lucas Almeida <lucasaclima03@gmail.com>
2022-03-30 10:16:20 -0300

    criação da entidade de usuario

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  senha: string;
+
+  @Column()
+  telefone: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export { User };
